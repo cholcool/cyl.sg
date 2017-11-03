53b5bd911bd5a64ff98ddfd4f09fa0300e2a8f45
cholcool <cholcool123@gmail.com>
2017-11-03 10:49:37 +0700

    make booking

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,3 +3,5 @@ $(".on_our_serviece").click(function() {
     scrollTop: $(".onourservice").offset().top
   }, 1000, "easeInOutExpo");
 });
+var day = new Date();
+document.getElementById("copyrightyear").innerHTML = day.getFullYear();
