5bbfbdadc45f3ef436cfdd4c2b7e5ec09fde6780
cholcool <cholcool123@gmail.com>
2017-11-06 10:39:24 +0700

    new slide, delete Online Scheduling Instructions

--- assets/js/common.js
+++ assets/js/common.js
@@ -5,3 +5,36 @@ $(".on_our_serviece").click(function() {
 });
 var day = new Date();
 document.getElementById("copyrightyear").innerHTML = day.getFullYear();
+(function() {
+    
+    // get references to select list and display text box
+    var sel = document.getElementById('scripts');
+    var el = document.getElementById('display');
+
+
+    function getSelectedOption(sel) {
+        var opt;
+        for ( var i = 0, len = sel.options.length; i < len; i++ ) {
+            opt = sel.options[i];
+            if ( opt.selected === true ) {
+                break;
+            }
+        }
+        return opt;
+    }
+
+    // assign onclick handlers to the buttons
+    document.getElementById('showVal').onclick = function () {
+        el.value = sel.value;    
+    }
+    
+    document.getElementById('showTxt').onclick = function () {
+        // access text property of selected option
+        el.value = sel.options[sel.selectedIndex].text;
+    }
+
+    document.getElementById('doLoop').onclick = function () {
+        var opt = getSelectedOption(sel);
+        el.value = opt.value;
+    }
+}());
